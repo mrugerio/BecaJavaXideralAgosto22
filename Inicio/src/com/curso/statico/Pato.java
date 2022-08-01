5e86f8ebfada4e6fa1a22e0285a8a3de7bb19aa2
Miguel Rugerio <mrugerio@gmail.com>
2022-08-01 18:11:43 -0500

    Bloques static

--- Inicio/src/com/curso/statico/Pato.java
+++ Inicio/src/com/curso/statico/Pato.java
@@ -4,6 +4,10 @@ import java.util.Objects;
 
 public class Pato{
 	
+	static {
+		System.out.println("Soy un bloque static 1");
+	}
+	
 	//Variable de Instancia de clase u Objeto
 	String nombre;
 	int edad;
@@ -14,12 +18,17 @@ public class Pato{
 	//SobreCarga (Overloading)
 	Pato() {
 		contador++; //contador = contador + 1
+		System.out.println("Paso por constructor Default");
 	}
 
 	Pato(String nombre, int edad) {
 		this.nombre = nombre;
 		this.edad = edad;
 		contador++;
+	}
+	
+	{
+		System.out.println("Soy un bloque de Instancia");
 	}
 
 	//Método de instancia
@@ -29,6 +38,10 @@ public class Pato{
 	
 	static int obtenerContador() {
 		return contador;
+	}
+	
+	static {
+		System.out.println("Soy un bloque static 2");
 	}
 	
 }
---
+++ Inicio/src/com/curso/statico/Principal3.java
@@ -0,0 +1,14 @@
+package com.curso.statico;
+
+public class Principal3 {
+
+	public static void main(String[] args) {
+		
+		Pato pato1 = new Pato();
+		Pato pato2 = new Pato();
+		Pato pato999 = new Pato();
+
+
+	}
+
+}
