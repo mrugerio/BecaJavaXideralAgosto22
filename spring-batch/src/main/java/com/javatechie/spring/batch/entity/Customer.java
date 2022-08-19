a41309df89e6ebd7e375251c1087973f666891a2
Miguel Rugerio <mrugerio@gmail.com>
2022-08-19 14:29:35 -0500

    Proyecto Spring Batch

---
+++ spring-batch/src/main/java/com/javatechie/spring/batch/entity/Customer.java
@@ -0,0 +1,100 @@
+package com.javatechie.spring.batch.entity;
+
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CUSTOMERS_INFO")
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @Column(name = "CUSTOMER_ID")
+    private int id;
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+    @Column(name = "LAST_NAME")
+    private String lastName;
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "GENDER")
+    private String gender;
+    @Column(name = "CONTACT")
+    private String contactNo;
+    @Column(name = "COUNTRY")
+    private String country;
+    @Column(name = "DOB")
+    private String dob;
+    
+    public Customer() {
+    }
+	public Customer(int id, String firstName, String lastName, String email, String gender, String contactNo,
+			String country, String dob) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.gender = gender;
+		this.contactNo = contactNo;
+		this.country = country;
+		this.dob = dob;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getContactNo() {
+		return contactNo;
+	}
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+    
+}
