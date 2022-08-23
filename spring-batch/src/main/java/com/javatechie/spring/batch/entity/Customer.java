244ae80a516516c8d15fc8f301e36a56023dd7cf
Miguel Rugerio <mrugerio@gmail.com>
2022-08-23 12:33:03 -0500

    Se implemento Lombok

--- spring-batch/src/main/java/com/javatechie/spring/batch/entity/Customer.java
+++ spring-batch/src/main/java/com/javatechie/spring/batch/entity/Customer.java
@@ -1,8 +1,8 @@
 package com.javatechie.spring.batch.entity;
 
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,9 +11,9 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "CUSTOMERS_INFO")
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
 
     @Id
@@ -33,68 +33,5 @@ public class Customer {
     private String country;
     @Column(name = "DOB")
     private String dob;
-    
-    public Customer() {
-    }
-	public Customer(int id, String firstName, String lastName, String email, String gender, String contactNo,
-			String country, String dob) {
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.gender = gender;
-		this.contactNo = contactNo;
-		this.country = country;
-		this.dob = dob;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	public String getContactNo() {
-		return contactNo;
-	}
-	public void setContactNo(String contactNo) {
-		this.contactNo = contactNo;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	public String getDob() {
-		return dob;
-	}
-	public void setDob(String dob) {
-		this.dob = dob;
-	}
-
     
 }
